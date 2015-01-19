724772813224ce09a3e732a88af470caf3e735cb
aubryta <aubry.ta@gmail.com>
2015-01-19 10:27:17 +0100

    Création projet
    Lors de l'inscription, l'ajout de l'utilisateur dans la base de données
    ne marche pas

---
+++ TPGP/SpeedDating/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpeedDating.MvcApplication" Language="C#" %>
---
+++ TPGP/SpeedDating/Models/InscriptionModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+  
+namespace SpeedDating.Models
+{
+    public class InscriptionModel
+    {
+        [Required(ErrorMessage = "Le nom est obligatoire")]
+        public string Nom { get; set; }
+        [Required(ErrorMessage = "Le prénom est obligatoire")]
+        public string Prenom { get; set; }
+        [Required(ErrorMessage = "L'adresse mail est obligatoire")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Le mot de passe est obligatoire")]
+        public string MDP { get; set; }
+    }
+}
